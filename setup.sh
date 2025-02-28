3c0c11fbf2ef9688aa58b232cc1ce49f932d7559
Aniket Singh <66803426+AniketSinghRathour@users.noreply.github.com>
2025-03-01 01:17:16 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,11 +10,15 @@ export PATH="$HOME/.cargo/bin:$PATH"
 # Use Python 3.10 as default
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
 
+# Remove old Python packages
+python3 -m pip cache purge
+python3 -m pip uninstall -y numpy spacy thinc transformers huggingface_hub
+
 # Upgrade pip & setuptools
 python3 -m pip install --upgrade pip setuptools wheel
 
 # Install CPU-optimized PyTorch
-pip install torch==2.0.1 torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+pip install --no-cache-dir torch==2.0.1 torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 
 # Install spaCy model
 python3 -m spacy download en_core_web_sm
